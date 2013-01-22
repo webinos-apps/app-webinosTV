842af989bfdc95161ed4beee81e056e9502f92a7
Vito Macchia <vito.macchia@gmail.com>
2013-01-22 11:29:21 +0100

    added header panels to Phone UI

--- experiments/st21/webinosTV-testUI/app/view/phone/PanelsGrid.js
+++ experiments/st21/webinosTV-testUI/app/view/phone/PanelsGrid.js
@@ -29,64 +29,162 @@ Ext.define('webinosTV.view.phone.PanelsGrid', {
       xtype:'carousel',
 	items:
 	[
-	{//Container #0 (#0a and #0b)
-	  xtype:'tileslist',
-	  defaultType: 'sourcedevlistitem',//for source devices
-	  flex: 2.5,
-	  store: {
-	    fields: ['count', 'type', 'deviceName'],
-	    data: [
-	      {type: 'pc',  count: 100,deviceName:'My PC'},
-	      {type: 'tv',   count: 2,deviceName:'Living Room TV'},
-	      {type: 'phone', count: 0,deviceName:'My Phone'},
-	      {type: 'tablet', count: 24,deviceName:'Ralph\'s tablet'},
-	      {type: 'laptop', count: 24,deviceName:'Mike\'s laptop'},
-	      {type: 'tablet', count: 4,deviceName:'My Tablet'},
-	      {type: 'tv',   count: 6,deviceName:'Kitchen TV'}
+	{
+	  xtype:'container',
+	  height:'100%',
+	  layout:{
+	    type: 'vbox',
+	    align: 'center',
+	    pack: 'center'
+	  },
+	  items:[
+	    { //Headers  #0 (#0a and #0b)
+	      xtype:'container',
+	      width:'100%',
+	      cls:'title-container',
+	      layout:{
+		type: 'hbox',
+		align: 'center',
+		pack: 'center'
+	      },
+	      items:[
+		{ xtype: 'panel', html: 'Queue', padding:2, margin: 2, flex:1}, //#0a
+		{ xtype: 'panel', html: 'Source Device',padding:2, margin: 2,flex:1.5},//#0b
+	      ]
+	    },
+	    {//Container #0 (#0a and #0b)
+	      xtype:'tileslist',
+	      defaultType: 'sourcedevlistitem',//for source devices
+	      width:'100%',
+	      store:'tmpsrcdevstore-id'
+	    }
+	  ]
+	},
+	{
+	  xtype:'container',
+	  height:'100%',
+	  layout:{
+	    type: 'vbox',
+	    align: 'center',
+	    pack: 'center'
+	  },
+	  items:[
+	  { //Headers  #1
+	    xtype:'container',
+	    width:'100%',
+	    cls:'title-container',
+	    layout:{
+	      type: 'hbox',
+	      align: 'center',
+	      pack: 'center'
+	    },
+	    items:[
+	      { xtype: 'panel', html: 'Media Type',padding:2, margin: 2/*,flex:1.5*/},//#1
 	    ]
-	  }
+	  },
+	  {//Container #1 - Media type
+	    xtype:'tileslist',
+	    id:'mediaTypeList',
+	    defaultType: 'medialistitem',//for media
+	    width:'100%',
+// 	    flex: 1.5,
+	    store: {
+	      fields: ['type', 'mediaTypeName'],
+	      data: [
+		{type: 'music',mediaTypeName:'Mp3 collection'},
+		{type: 'channels',mediaTypeName:'DVB-T'},
+		{type: 'movies',mediaTypeName:'Videos'},
+		{type: 'images',mediaTypeName:'Pictures'},
+		{type: 'apps',mediaTypeName:'webinos app store'},
+		{type: 'docs',mediaTypeName:'Documents'}
+	      ]
+	    }
+	  }]
 	},
-	{//Container #1 - Media type
-	  xtype:'tileslist',
-	  id:'mediaTypeList',
-	  defaultType: 'medialistitem',//for media
-	  flex: 1.5,
-	  store: {
-	    fields: ['type', 'mediaTypeName'],
-	    data: [
-	      {type: 'music',mediaTypeName:'Mp3 collection'},
-	      {type: 'channels',mediaTypeName:'DVB-T'},
-	      {type: 'movies',mediaTypeName:'Videos'},
-	      {type: 'images',mediaTypeName:'Pictures'},
-	      {type: 'apps',mediaTypeName:'webinos app store'},
-	      {type: 'docs',mediaTypeName:'Documents'}
+	{
+	  xtype:'container',
+	  height:'100%',
+	  layout:{
+	    type: 'vbox',
+	    align: 'center',
+	    pack: 'center'
+	  },
+	  items:[
+	  { //Headers  #2
+	    xtype:'container',
+	    width:'100%',
+	    cls:'title-container',
+	    layout:{
+	      type: 'hbox',
+	      align: 'center',
+	      pack: 'center'
+	    },
+	    items:[
+	      { xtype: 'panel', html: 'Select Media',padding:2, margin: 2/*,flex:1.5*/},//#1
 	    ]
-	  }
+	  },
+	  {//Container #2 - playlist TODO: should change according with the media selected in #1
+	    xtype: 'mediaplaylist',
+	    width:'100%',
+ 	    height:'100%'
+	  }]
 	},
-	{//Container #2 - playlist TODO: should change according with the media selected in #1
-	  xtype: 'mediaplaylist',
-	  flex:3
-	},
-	{ //Container #3 - Display devices
-	  xtype:'tileslist',
-	  defaultType: 'displaydevlistitem',//for display devices
-// 	  flex: 1.5,
-	  store: {
-	    fields: ['type', 'deviceName'],
-	    data: [
-	      {type: 'pc', deviceName:'My PC'},
-	      {type: 'tv',  deviceName:'Living Room TV'},
-	      {type: 'phone',deviceName:'My Phone'},
-// 	      {type: 'tablet', deviceName:'Ralph\'s tablet'},
-// 	      {type: 'laptop',deviceName:'Mike\'s laptop'},
-	      {type: 'tablet',deviceName:'My Tablet'},
-	      {type: 'tv',  deviceName:'Kitchen TV'}
+	{
+	  xtype:'container',
+	  height:'100%',
+	  layout:{
+	    type: 'vbox',
+	    align: 'center',
+	    pack: 'center'
+	  },
+	  items:[
+	  { //Headers  #3
+	    xtype:'container',
+	    width:'100%',
+	    cls:'title-container',
+	    layout:{
+	      type: 'hbox',
+	      align: 'center',
+	      pack: 'center'
+	    },
+	    items:[
+	      { xtype: 'panel', html: 'Display Device',padding:2, margin: 2/*,flex:1.5*/},//#1
 	    ]
-	  }
+	  },
+	  { //Container #3 - Display devices
+	    xtype:'tileslist',
+	    defaultType: 'displaydevlistitem',//for display devices
+	    width:'100%',
+	    height:'100%',
+	    store: 'tmpdispdevstore-id'
+	  }]
 	},
+	{
+	  xtype:'container',
+	  height:'100%',
+	  layout:{
+	    type: 'vbox',
+	    align: 'center',
+	    pack: 'center'
+	  },
+	  items:[
+	  { //Headers  #4
+	    xtype:'container',
+	    width:'100%',
+	    cls:'title-container',
+	    layout:{
+	      type: 'hbox',
+	      align: 'center',
+	      pack: 'center'
+	    },
+	    items:[
+	      { xtype: 'panel', html: 'Play Mode',padding:2, margin: 2/*,flex:1.5*/},//#1
+	    ]
+	  },
 	{//Container #4 - Actions
 	  xtype: 'customsegbutton',
-	  flex:1.5,
+	  width:'100%',
+	  height:'100%',
 	  padding:1,
 	  layout:
 	  {
@@ -96,6 +194,7 @@ Ext.define('webinosTV.view.phone.PanelsGrid', {
 	    {xtype:'tilepanel', iconCls : 'play', text:'Play Now'},
 	    {xtype:'tilepanel', iconCls : 'queue', text:'Add to Queue'}
 	  ]
+	}]
 	}
       ]
     }
