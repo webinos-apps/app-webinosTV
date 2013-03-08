f588bd913061ca657edc22d39f64c905eb7c3210
Vito Macchia <vito.macchia@gmail.com>
2013-03-08 17:20:36 +0100

    Eyecandy: fixed playlist css and added a blue glowing shadow class for selected columns header

--- app/controller/SelectCategoryController.js
+++ app/controller/SelectCategoryController.js
@@ -5,27 +5,29 @@ Ext.define('webinosTV.controller.SelectCategoryController', {
 
   //TODO this controller should change shape depending on the selected media category
   config: {
-    control:{
+    control: {
       mcategory:
-      {
-        select:'mediaCategorySelected', //event = select, cb = mediaCategorySelected
-        deselect:'mediaCategoryDeselected'
-      }
+        {
+          select: 'mediaCategorySelected', //event = select, cb = mediaCategorySelected
+          deselect: 'mediaCategoryDeselected'
+        }
     },
     refs: {
-      smColumn:'#mediaSelectionColumn',
+      categColumn: '#categoriesColumn',
+      smColumn: '#mediaSelectionColumn',
       mplist: '#mediaPlaylist',
       mcategory: '#mediaCategoryList',
       mtargetdevs: '#targetDevicesList',
       mactions: '#actionsList'
     }
   },
-  
-  mediaCategoryDeselected:function(mediaCategoryList, record, eOpts){
+  mediaCategoryDeselected: function(mediaCategoryList, record, eOpts) {
 //     var mplist=this.getMplist();
-    var smColumn=this.getSmColumn();
-    var mtargetdevs=this.getMtargetdevs();
-    var mactions=this.getMactions();
+    var selectedColumn = this.getCategColumn();
+    selectedColumn.getAt(0).removeCls('selected-column-header');
+    var smColumn = this.getSmColumn();
+    var mtargetdevs = this.getMtargetdevs();
+    var mactions = this.getMactions();
 //     mplist.setItems({
 //       xtype:'tilepanel',
 //       cls:['tile-panel'],
@@ -41,43 +43,44 @@ Ext.define('webinosTV.controller.SelectCategoryController', {
     mactions.setMasked(true);
     mactions.setDisabled(false);
   },
-
-  mediaCategorySelected:function(mediaCategoryList, record, eOpts)
+  mediaCategorySelected: function(mediaCategoryList, record, eOpts)
   {
+    var selectedColumn = this.getCategColumn();
+    selectedColumn.getAt(0).addCls('selected-column-header');
 //     var mplist=this.getMplist();
-    var smColumn=this.getSmColumn();
+    var smColumn = this.getSmColumn();
 
 //     console.log("mediaCategorySelected",mplist)
 //     var mcategoryList=this.getMcategory();
 //     console.log("mediaCategorySelected",mcategoryList.getSelectionCount(),record)
 //      console.log("selected",record,record.get('mediaCategoryName'),record.get('category'));
-    var mediaCategory=record.get('category');
-    switch(mediaCategory)
+    var mediaCategory = record.get('category');
+    switch (mediaCategory)
     {
       case 'movies':
 //  console.log("selected movies");
-    smColumn.showVideos();
-    break;
+        smColumn.showVideos();
+        break;
       case 'music':
 //  console.log("selected mp3");
-    smColumn.showMusic();
-    break;
+        smColumn.showMusic();
+        break;
       case 'images':
 //  console.log("selected pictures");
-    smColumn.showImages();
-    break;
+        smColumn.showImages();
+        break;
       case 'channels':
 //  console.log("selected TV");
-    smColumn.showTV();
-    break;
+        smColumn.showTV();
+        break;
       case 'apps':
 //  console.log("selected Apps");
-    smColumn.showApps();
-    break;
+        smColumn.showApps();
+        break;
       case 'docs':
 //  console.log("selected Documents");
-    smColumn.showDocuments();
-    break;
+        smColumn.showDocuments();
+        break;
     }
 //     mplist.setMasked(false);
 //     mplist.setDisabled(true);
--- app/controller/SelectPlayModeController.js
+++ app/controller/SelectPlayModeController.js
@@ -5,26 +5,29 @@ Ext.define('webinosTV.controller.SelectPlayModeController', {
 
   //TODO actions depending on media type - right now it's just masking/unmasking
   config: {
-    control:{
+    control: {
       actionList:
-      {
-        select:'actionSelected',
-        deselect:'actionDeSelected'
-      }
+        {
+          select: 'actionSelected',
+          deselect: 'actionDeSelected'
+        }
     },
     refs: {
+      actionsColumn: '#actionControlsColumn',
       actionList: '#actionsList',
       tdevList: '#targetDevicesList'
     }
   },
-
-
-  actionSelected:function(actionList, record, eOpts)
+  actionSelected: function(actionList, record, eOpts)
   {
+    var selectedColumn = this.getActionsColumn();
+    selectedColumn.getAt(0).addCls('selected-column-header');
     record.get('action')("play");
   },
-  actionDeSelected:function(actionList, record, eOpts)
+  actionDeSelected: function(actionList, record, eOpts)
   {
+    var selectedColumn = this.getActionsColumn();
+    selectedColumn.getAt(0).removeCls('selected-column-header');
     record.get('action')("stop");
   }
 
